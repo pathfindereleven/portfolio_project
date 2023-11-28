cab593d29b4cb06812192bfc57da3b8d5182bc26
pathfindereleven <njmizerny@rocketmail.com>
2023-11-28 15:25:58 -0800

    pie grapg pop up

--- Demo_Day/invreport.py
+++ Demo_Day/invreport.py
@@ -4,7 +4,8 @@ import time
 from pathlib import Path
 from datetime import datetime
 import sqlite3
-
+import matplotlib.pyplot as plt
+import os
 sset_list = ['MID', '10E', 'BRO', 'DGM', 'DMU', 'ELD', 'GTC', 'MAT', 'MOM', 'NEO', 'ONE', 'RTR', 'SNC', 'VOW', 'WOE']
 
 # build db conection
@@ -38,3 +39,16 @@ report_df =  pd.DataFrame(zip(sset_list, total_cards, totals_price), columns=['s
 print(report_df)
 x = report_df['totalprice'].sum()
 print(f' total inventory market price is {x}')
+# create pie graph df of top 7 and combine all other data to other
+pie_df = report_df.sort_values(by='totalprice', ascending=False)
+pie_df2 = pie_df.iloc[0:7] 
+other = pie_df.iloc[7:len(pie_df)]
+otherprice= other["totalprice"].sum()
+othercards = other["cards"].sum()
+pie_df2.loc[-1] = ['other', othercards, otherprice]
+my_data = pie_df2['totalprice']
+my_labels = pie_df2['set'] 
+plt.pie(my_data, labels=my_labels, autopct="%1.1f%%", radius =1.4)
+
+plt.savefig(r"C:\Users\pc\OneDrive\Desktop\portfolio_project\Demo_Day\Resources\pie.png")
+os.startfile(r"C:\Users\pc\OneDrive\Desktop\portfolio_project\Demo_Day\Resources\pie.png")
